909d678bcf10693dd3684ca92852a31968cebdbf
tctinsama <125227798+tctinsama@users.noreply.github.com>
2023-12-23 00:36:12 +0700

    rec.c

--- rec.c
+++ rec.c
@@ -1,29 +1,62 @@
-#include<stdlib.h>
- #include<stdio.h>
- #include<string.h>
- #include<unistd.h>
- #include<sys/types.h>
- #include<sys/ipc.h>
- #include<sys/msg.h>
- struct my_msg{
-         long int msg_type;
-         char some_text[BUFSIZ];
- };
- int main()
- {
-         int running=1;
-         int msgid;
-         struct my_msg some_data;
-         long int msg_to_rec=0;
-         msgid=msgget((key_t)14534,0666|IPC_CREAT);
-         while(running)
-         {
-                 msgrcv(msgid,(void *)&some_data,BUFSIZ,msg_to_rec,0);                 
-                 printf("Data received: %s\n",some_data.some_text);
-                 if(strncmp(some_data.some_text,"end",3)==0)
-                 {
-                         running=0;
-                 }
-         }
-          msgctl(msgid,IPC_RMID,0);
- }
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+#include <ncurses.h>
+
+#define MAX_TEXT 512 // Kích thước tối đa của tin nhắn
+
+struct my_msg {
+    long int msg_type;
+    char file_data[MAX_TEXT];
+};
+
+int main() {
+    int running = 1;
+    int msgid;
+    struct my_msg some_data;
+    char filename[MAX_TEXT];
+
+    initscr(); // Khởi tạo ncurses
+    printw("Enter the filename to save the received file: ");
+    refresh();
+    getstr(filename); // Nhập tên file
+
+    FILE *file = fopen(filename, "w");
+    if (file == NULL) {
+        printw("Error in opening file\n");
+        exit(EXIT_FAILURE);
+    }
+
+    msgid = msgget((key_t)14534, 0666 | IPC_CREAT); // Tạo hoặc kết nối hàng đợi tin nhắn
+    if (msgid == -1) {
+        printw("Error in creating queue\n");
+        exit(EXIT_FAILURE);
+    }
+
+    while (running) {
+        if (msgrcv(msgid, (void *)&some_data, MAX_TEXT, 1, 0) == -1) {
+            printw("Error in receiving message\n");
+            exit(EXIT_FAILURE);
+        }
+
+        fprintf(file, "%s", some_data.file_data);
+
+        if (strncmp(some_data.file_data, "end", 3) == 0) {
+            running = 0;
+        }
+    }
+
+    fclose(file);
+
+    if (msgctl(msgid, IPC_RMID, 0) == -1) {
+        printw("Error in removing queue\n");
+        exit(EXIT_FAILURE);
+    }
+
+    endwin(); // Kết thúc ncurses
+    exit(EXIT_SUCCESS);
+}
