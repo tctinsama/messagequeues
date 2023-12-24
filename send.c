60060d295ff65802c500757442da5acc09604a5e
tctinsama <125227798+tctinsama@users.noreply.github.com>
2023-12-24 15:18:36 +0700

    send.c

--- send.c
+++ send.c
@@ -1,59 +1,72 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
 #include <sys/types.h>
 #include <sys/ipc.h>
 #include <sys/msg.h>
-#include <ncurses.h>
 
-#define MAX_TEXT 512 // Kích thước tối đa của tin nhắn
+#define MAX_MSG_SIZE 1024
+#define MSG_KEY 1234
 
-struct my_msg {
-    long int msg_type;
-    char file_data[MAX_TEXT];
+struct message {
+    long msg_type;
+    char msg_text[MAX_MSG_SIZE];
 };
 
-int main() {
-    int running = 1;
-    int msgid;
-    struct my_msg some_data;
-    char filename[MAX_TEXT];
-
-    initscr(); // Khởi tạo ncurses
-    printw("Enter the filename to send: ");
-    refresh();
-    getstr(filename); // Nhập tên file
-
-    FILE *file = fopen(filename, "r");
+void send_file(int msgid, const char *file_path) {
+    struct message msg;
+    FILE *file = fopen(file_path, "r");
     if (file == NULL) {
-        printw("Error in opening file\n");
+        perror("File opening failed");
         exit(EXIT_FAILURE);
     }
 
-    msgid = msgget((key_t)14534, 0666 | IPC_CREAT); // Tạo hoặc kết nối hàng đợi tin nhắn
-    if (msgid == -1) {
-        printw("Error in creating queue\n");
-        exit(EXIT_FAILURE);
-    }
+    msg.msg_type = 1;
+    size_t bytes_read;
 
-    while (running) {
-        if (fgets(some_data.file_data, MAX_TEXT, file) == NULL) {
-            strcpy(some_data.file_data, "end");
-            running = 0;
-        }
-
-        some_data.msg_type = 1;
-
-        if (msgsnd(msgid, (void *)&some_data, MAX_TEXT, 0) == -1) {
-            printw("Msg not sent\n");
+    // Đọc file và gửi dữ liệu
+    while ((bytes_read = fread(msg.msg_text, sizeof(char), MAX_MSG_SIZE, file)) > 0) {
+        if (msgsnd(msgid, &msg, bytes_read, 0) == -1) {
+            perror("msgsnd");
             exit(EXIT_FAILURE);
         }
+    }
 
-        usleep(1000);
+    // Gửi một message để thông báo việc gửi file đã hoàn thành
+    strcpy(msg.msg_text, "EOF");
+    if (msgsnd(msgid, &msg, strlen("EOF") + 1, 0) == -1) {
+        perror("msgsnd");
+        exit(EXIT_FAILURE);
     }
 
     fclose(file);
-    endwin(); // Kết thúc ncurses
-    exit(EXIT_SUCCESS);
+}
+
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        printf("Usage: %s <file_path>\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+
+    int msgid;
+    key_t key;
+
+    // Tạo key cho message queue
+    key = ftok("msg_queue_key", MSG_KEY);
+    if (key == -1) {
+        perror("ftok");
+        exit(EXIT_FAILURE);
+    }
+
+    // Tạo hoặc kết nối vào message queue
+    msgid = msgget(key, 0666 | IPC_CREAT);
+    if (msgid == -1) {
+        perror("msgget");
+        exit(EXIT_FAILURE);
+    }
+
+    // Gửi file được chỉ định trong tham số dòng lệnh
+    send_file(msgid, argv[1]);
+
+    return 0;
 }
